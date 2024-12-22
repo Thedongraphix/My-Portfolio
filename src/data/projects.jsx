7424ca9736feccd35b97c5b41dcaf1de7cad14f1
Thedongraphix <chrisoketchcn@gmail.com>
2024-12-22 08:18:22 +0300

    Projects revamp

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -8,11 +8,5 @@ export const projects = [
     link: { href: 'https://edupaydefi.vercel.app', label: 'EdupayDefi' },
     logo: logoPlanetaria,
   },
-  {
-    name: 'zenlipa',
-    description: 'A giving and campaign tracking platform for religious and charity organizations.',
-    link: { href: 'https://zenlipa.co.ke', label: 'Zenlipa' },
-    logo: logoAnimaginary,
-  },
 ]
 
