c69ea4aec0fefb129dcdc5e95b07f15628b96514
Thedongraphix <chrisoketchcn@gmail.com>
2025-01-07 22:50:06 +0300

    Adding subscription emoji

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -6,14 +6,14 @@ export default function ThankYou() {
   return (
     <>
       <Head>
-        <title>You’re subscribed to Chris Oketch </title>
+        <title>Subscription</title>
         <meta
           name="description"
           content="Thanks for subscribing to my newsletter."
         />
       </Head>
       <SimpleLayout
-        title="Thanks for subscribing."
+        title="Thanks for subscribing🎉"
         intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
       />
     </>
